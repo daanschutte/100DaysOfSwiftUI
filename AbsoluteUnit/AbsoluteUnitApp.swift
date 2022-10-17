f4764ceffc0d84be703406f471c46a1e17d2bc36
Daan Schutte <daan.schutte@icloud.com>
2022-10-17 11:56:36 +0300

    Initial Commit

---
+++ AbsoluteUnit/AbsoluteUnitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsoluteUnitApp.swift
+//  AbsoluteUnit
+//
+//  Created by Daan Schutte on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AbsoluteUnitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
