77b9d51856d45f5016ee650c21a7e52cda8f472e
Daan Schutte <daan.schutte@icloud.com>
2022-10-19 07:19:11 +0300

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Daan Schutte on 19/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
