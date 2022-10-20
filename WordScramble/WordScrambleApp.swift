848611ef55a8fa6fc868c7c64ee60af37acb563a
Daan Schutte <daan.schutte@icloud.com>
2022-10-20 16:39:59 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Daan Schutte on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
