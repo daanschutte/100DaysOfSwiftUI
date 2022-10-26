1ca77aaa9b991d7e0640c7f5d2092ae3677f9679
Daan Schutte <daan.schutte@icloud.com>
2022-10-26 17:17:15 +0300

    Day 35: Extract views to custom views

--- TopTables/TopTables/ContentView.swift
+++ TopTables/TopTables/ContentView.swift
@@ -20,123 +20,121 @@ extension Question: Identifiable {
     }
 }
 
-struct ContentView: View {
-    @State private var baseNumber = 1
+
+struct SettingsView: View {
+    @Binding var base: Int
+    @Binding var gameInProgress: Bool
+    @Binding var questions: [Question]
+    
     @State private var numQuestions = 5
-    @State private var questions = [Question]()
-    
-    @State private var gameInProgress = false
-    @State private var showingScore = false
-    @State private var showingResults = false
-    
-    @State private var randomTable = false
-    
-    @State private var total = 0
+    @State private var random = false
     
     var body: some View {
-        
-        // TODO: Extract into own view
-        if !gameInProgress {
-            VStack {
-                HStack {
-                    Text("x Table")
-                        .font(.headline.bold())
-                        .padding(.trailing)
-                    
-                    if !randomTable {
-                        Stepper(
-                            value: $baseNumber,
-                            in: 1...12,
-                            step: 1) {
-                                Text("\(baseNumber)")
-                                    .font(.subheadline)
-                                    .frame(maxWidth: .infinity, alignment: .center)
-                            }.padding(.horizontal)
-                    }
-                    
-                    Toggle(isOn: $randomTable) {
-                        Text("Random")
-                            .font(.subheadline)
-                            .frame(maxWidth: .infinity, alignment: .bottomTrailing)
-                    }.padding(.leading)
-                }
-                .padding(.horizontal)
-                
-                HStack {
-                    Stepper(value: $numQuestions, in: 5...15, step: 5) {
-                        HStack{
-                            Text("Questions")
+        VStack {
+            HStack {
+                if !random {
+                    Stepper(
+                        value: $base,
+                        in: 1...12,
+                        step: 1) {
+                            Text("\(base) x Table")
                                 .font(.headline.bold())
-                            Spacer()
-                            Text("\(numQuestions)")
-                                .font(.subheadline)
                         }
-                    }
-                    .padding(.horizontal)
-                    Spacer()
                 }
-                Button("Start") {
-                    startGame()
-                }
-                .buttonStyle(.borderedProminent)
-                .padding()
+                
+                Toggle(isOn: $random) {
+                    Text("Random")
+                        .font(.subheadline)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }.padding(.leading)
             }
-        }
-        
-        // TODO: this should be if/else or some better construct
-        if gameInProgress {
-            VStack {
-                List($questions) { $question in
-                    HStack {
-                        Text(question.problem)
-                        TextField("?", text: $question.userAttempt)
-                            .disableAutocorrection(true)
-                            .keyboardType(.numberPad)
-                        if showingResults && !question.userAttempt.isEmpty {
-                            Image(systemName: question.isCorrect() ? "hand.thumbsup" : "hand.thumbsdown")
-                        }
-                    }
-                    
-                }
-                .clipShape(RoundedRectangle(cornerRadius: 5))
-                
-                Button("Calculate Score") {
-                    calculateScore()
-                    showingScore = true
-                    showingResults = true
-                }
-                .buttonStyle(.borderedProminent)
-                .alert("Score", isPresented: $showingScore) {
-                } message: {
-                    Text("You have \(total)/\(questions.count) correct answers!")
-                }
-                .padding(15)
-                
-                
-                Spacer()
-                Button("Reset Game", role: .destructive) {
-                    reset()
+            .padding(.horizontal)
+            
+            HStack {
+                Stepper(value: $numQuestions, in: 5...15, step: 5) {
+                    Text("\(numQuestions) questions")
+                        .font(.headline.bold())
+                        .frame(maxWidth: .infinity, alignment: .leading)
                 }
                 .padding(.horizontal)
             }
+            
+            Button("Start") {
+                startGame()
+            }
+            .buttonStyle(.borderedProminent)
+            .padding()
         }
-    }
-    
-    func startGame() {
-        gameInProgress = true
-        createQuestionList(randomTable)
     }
     
     func createQuestionList(_ random: Bool = false) {
         questions.removeAll()
         for n in 1...numQuestions {
-            let base = randomTable ? Int.random(in: 1...12) : baseNumber
+            let base = random ? Int.random(in: 1...12) : base
+            let n = random ? Int.random(in: 1...12) : n
             let question = Question(problem: "\(base) x \(n) =", answer: base * n)
             questions.append(question)
         }
         
         if random {
             questions.shuffle()
+        }
+    }
+    
+    func startGame() {
+        gameInProgress = true
+        createQuestionList(random)
+    }
+}
+
+struct GameView: View {
+    @Binding var base: Int
+    @Binding var gameInProgress: Bool
+    @Binding var questions: [Question]
+    
+    @State private var showingScore = false
+    @State private var showingResults = false
+    @State private var total = 0
+    
+    var reset: () -> Void
+    
+    var body: some View {
+        VStack {
+            List($questions) { $question in
+                HStack {
+                    Text(question.problem)
+                    TextField("?", text: $question.userAttempt)
+                        .disableAutocorrection(true)
+                        .keyboardType(.numberPad)
+                    if showingResults && !question.userAttempt.isEmpty {
+                        Image(systemName: question.isCorrect() ? "hand.thumbsup" : "hand.thumbsdown")
+                    }
+                }
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 5))
+            
+            
+            Button("Calculate Score") {
+                calculateScore()
+                showingScore = true
+                showingResults = true
+            }
+            .buttonStyle(.borderedProminent)
+            .alert("Score", isPresented: $showingScore) {
+            } message: {
+                Text("You have \(total)/\(questions.count) correct answers!")
+            }
+            .padding(15)
+            
+            
+            Spacer()
+            Button("Reset Game", role: .destructive) {
+                total = 0
+                showingResults = false
+                
+                reset()
+            }
+            .padding(.horizontal)
         }
     }
     
@@ -147,13 +145,31 @@ struct ContentView: View {
             }
         }
     }
+}
+
+struct ContentView: View {
+    @State private var base = 1
+    @State private var gameInProgress = false
+    @State private var questions = [Question]()
+        
+    var body: some View {
+        
+        NavigationStack {
+            VStack {
+                if gameInProgress {
+                    GameView(base: $base, gameInProgress: $gameInProgress, questions: $questions, reset: reset)
+                } else  {
+                    SettingsView(base: $base, gameInProgress: $gameInProgress, questions: $questions)
+                }
+            }.navigationTitle(Text("Table Monster"))
+        }
+    }
     
     func reset() {
         gameInProgress = false
-        showingResults = false
-        baseNumber = 1
-        total = 0
+        base = 1
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
