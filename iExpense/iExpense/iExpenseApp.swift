dde36eb2109d773fb40e6c070008518c8badc07f
Daan Schutte <daan.schutte@icloud.com>
2022-10-28 10:26:47 +0300

    Day 36: create iExpense project

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Daan Schutte on 28/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
