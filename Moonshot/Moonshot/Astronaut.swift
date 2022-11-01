c09d146b0fcd2cdfa470bde16ae0769d57faff8d
Daan Schutte <daan.schutte@icloud.com>
2022-11-01 20:51:17 +0300

    Day 40: Add assets, json and create json decoder extension

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Daan Schutte on 01/11/2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
