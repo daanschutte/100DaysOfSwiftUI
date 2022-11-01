7d65abef0c08cda589e1f6c4cd4cfcb1c7f8177e
Daan Schutte <daan.schutte@icloud.com>
2022-11-01 14:58:25 +0300

    Day 39: Create project, ScrollView, Codable

---
+++ Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Daan Schutte on 01/11/2022.
+//
+
+import SwiftUI
+
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
+struct ContentView: View {
+    var body: some View {
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
+                }
+            }
+            """
+        
+            let data = Data(input.utf8)
+            
+            if let user = try? JSONDecoder().decode(User.self, from: data) {
+                print(user.address.street)
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Daan Schutte on 01/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
