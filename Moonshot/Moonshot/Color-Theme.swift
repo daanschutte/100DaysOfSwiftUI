f14a8f41f9a4e83d7b1f552ef214a4fd6a62cc4a
Daan Schutte <daan.schutte@icloud.com>
2022-11-04 19:16:39 +0300

    Day 41: Add astronaut view

--- Moonshot/Moonshot/Color-Theme.swift
+++ Moonshot/Moonshot/Color-Theme.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 extension ShapeStyle where Self == Color {
     static var darkBackGround: Color {
-        Color(red: 0.1, green: 0.2, blue: 0.3)
+        Color(red: 0.1, green: 0.1, blue: 0.2)
     }
     
     static var lightBackground: Color {
