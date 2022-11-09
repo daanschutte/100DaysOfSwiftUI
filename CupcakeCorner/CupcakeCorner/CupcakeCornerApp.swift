568aecff08cb3652e176dea9e42487eabbf2a348
Daan Schutte <daan.schutte@icloud.com>
2022-11-09 18:14:50 +0300

    Day 49: Create CupcakeCorner project

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Daan Schutte on 09/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
