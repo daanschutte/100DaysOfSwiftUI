3be84010a6eadc73afe44de2b26dc90bb8f722f2
Daan Schutte <daan.schutte@icloud.com>
2022-11-19 16:29:22 +0300

    Day 53: Create project, @Binding

---
+++ BookWorm/BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Daan Schutte on 19/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
