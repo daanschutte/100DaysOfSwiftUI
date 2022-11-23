31f4abf1733b9090a5fe7baaac8220f850c3d4b6
Daan Schutte <daan.schutte@icloud.com>
2022-11-23 16:16:15 +0300

    Day 57: Create data model and import to context

---
+++ CoreDataProject/CoreDataProject/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Daan Schutte on 23/11/2022.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
