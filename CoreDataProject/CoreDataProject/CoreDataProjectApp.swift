1382e2a699943356d4e16d33b79838ba12963546
Daan Schutte <daan.schutte@icloud.com>
2022-11-23 16:09:56 +0300

    Day 57: Create project

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Daan Schutte on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
