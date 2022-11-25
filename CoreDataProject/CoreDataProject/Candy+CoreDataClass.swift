3e660f9f21fc02afb1bee694d60a17b64ca326eb
Daan Schutte <daan.schutte@icloud.com>
2022-11-25 21:42:32 +0300

    Day 58: One-to-many relationships with Core Data, SwiftUI, and @FetchRequest

---
+++ CoreDataProject/CoreDataProject/Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Daan Schutte on 25/11/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ CoreDataProject/CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Daan Schutte on 25/11/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
