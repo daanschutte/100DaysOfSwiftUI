10e26b029a18f748838e6ff85beb17557200a0e9
Daan Schutte <daan.schutte@icloud.com>
2022-11-26 15:14:14 +0300

    Day 59: Challenge 2

--- CoreDataProject/CoreDataProject/FilteredList.swift
+++ CoreDataProject/CoreDataProject/FilteredList.swift
@@ -9,6 +9,14 @@ import CoreData
 import SwiftUI
 
 struct FilteredList<T: NSManagedObject, Content: View>: View {
+    enum FilteredListPredicates: String {
+        case beginsWith = "BEGINSWITH"
+        case contains = "CONTAINS"
+        case endsWith = "ENDSWITH"
+        case like = "LIKE"
+        case matches = "MATCHES"
+    }
+    
     @FetchRequest var fetchRequest: FetchedResults<T>
     
     let content: (T) -> Content
@@ -19,10 +27,10 @@ struct FilteredList<T: NSManagedObject, Content: View>: View {
         }
     }
     
-    init(predicate: String, filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+    init(predicate: FilteredListPredicates, filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
         _fetchRequest = FetchRequest<T>(
             sortDescriptors: [],
-            predicate: NSPredicate(format: "%K \(predicate) %@", filterKey, filterValue)
+            predicate: NSPredicate(format: "%K \(predicate.rawValue) %@", filterKey, filterValue)
         )
         
         self.content = content
