7c74b217d3c0bc3aed5eae4b9516c2bf10a762ff
Daan Schutte <daan.schutte@icloud.com>
2022-12-09 17:00:15 +0300

    Day 61: Create data model

---
+++ FriendFace/FriendFace/DataController.swift
@@ -0,0 +1,26 @@
+//
+//  DataController.swift
+//  FriendFace
+//
+//  Created by Daan Schutte on 09/12/2022.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "FriendFace")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core data failed to load: \(error.localizedDescription)")
+                return
+            }
+            
+            self.container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        }
+    }
+}
+
+
