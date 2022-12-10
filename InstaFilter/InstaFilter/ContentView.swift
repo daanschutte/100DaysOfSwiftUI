8aaefc567bd1efbdb8c5cffb7b6de1fde508e92f
Daan Schutte <daan.schutte@icloud.com>
2022-12-10 12:27:14 +0300

    Day 62: ConfirmationDialog

--- InstaFilter/InstaFilter/ContentView.swift
+++ InstaFilter/InstaFilter/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
-        }
-        .padding()
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                showingConfirmation = true
+            }
+            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+                Button("Red") { backgroundColor = .red }
+                Button("Green") { backgroundColor = .green}
+                Button("Blue") { backgroundColor = .blue}
+                Button("Cancel", role: .cancel) { }
+            } message: {
+                Text("Select new color")
+            }
     }
 }
 
