1e9f6909c045ea0618ecbbbf4c2a38089eba75ab
Daan Schutte <daan.schutte@icloud.com>
2022-12-10 12:20:58 +0300

    Day 62: Create InstaFilter project

---
+++ InstaFilter/InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Daan Schutte on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
