34d36e067e653a93cca46c1e7b1d341143abbb20
Daan Schutte <daan.schutte@icloud.com>
2022-12-12 18:40:53 +0300

    Day 68:  Create Bucketlist project

---
+++ Bucketlist/Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by Daan Schutte on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
