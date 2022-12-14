490912d212ff7556509c8cee653c1f117c404c88
Daan Schutte <daan.schutte@icloud.com>
2022-12-14 18:58:03 +0300

    Day 69: Reset project

--- Bucketlist/Bucketlist/ContentView.swift
+++ Bucketlist/Bucketlist/ContentView.swift
@@ -7,39 +7,8 @@
 
 import SwiftUI
 
-enum LoadingState {
-    case loading, success, failed
-}
-
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-struct SuccessView: View {
-    var body: some View {
-        Text("Success!")
-    }
-}
-struct FailedView: View {
-    var body: some View {
-        Text("Failed")
-    }
-}
-
 struct ContentView: View {
-    let loadingState = LoadingState.loading
-    
-    var body: some View {
-        switch loadingState {
-            case .loading:
-                LoadingView()
-            case .success:
-                SuccessView()
-            case .failed:
-                FailedView()
-        }
-    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
