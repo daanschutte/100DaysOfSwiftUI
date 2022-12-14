d2cf635234b924694e6405a2e861e90312158067
Daan Schutte <daan.schutte@icloud.com>
2022-12-14 19:10:44 +0300

    Day 69: Integrating MapKit with SwiftUI

--- Bucketlist/Bucketlist/ContentView.swift
+++ Bucketlist/Bucketlist/ContentView.swift
@@ -5,10 +5,42 @@
 //  Created by Daan Schutte on 12/12/2022.
 //
 
+import MapKit
 import SwiftUI
 
-struct ContentView: View {
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
 
+struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+        
+    ]
+    
+    
+    var body: some View {
+        NavigationStack {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                        Circle()
+                            .stroke(.red, lineWidth: 3)
+                        Text(location.name)
+                    }
+                }
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
