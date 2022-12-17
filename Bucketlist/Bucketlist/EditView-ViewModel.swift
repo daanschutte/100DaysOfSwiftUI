b31a6214d401c6b21e36c6cb3bb1794a1cbd3835
Daan Schutte <daan.schutte@icloud.com>
2022-12-17 11:53:13 +0300

    Day 73: Begin moving EditView logic to ViewModel

---
+++ Bucketlist/Bucketlist/EditView-ViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  EditView-ViewModel.swift
+//  Bucketlist
+//
+//  Created by Daan Schutte on 17/12/2022.
+//
+
+import Foundation
+
+extension EditView {
+    @MainActor class ViewModel: ObservableObject {
+        enum LoadingState {
+            case loading, loaded, failed
+        }
+        
+        @Published var loadingState: LoadingState
+        @Published var pages: [Page]
+        
+        init() {
+            self.loadingState = .loading
+            self.pages = []
+        }
+        
+        func fetchNearbyPlaces(location: Location) async {
+            let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+            
+            guard let url = URL(string: urlString) else {
+                print("Bad URL: \(urlString)")
+                return
+            }
+            
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url)
+                let items = try JSONDecoder().decode(Result.self, from: data)
+                pages = items.query.pages.values.sorted()
+                loadingState = .loaded
+            } catch {
+                loadingState = .failed
+            }
+            
+        }
+    }
+}
--- Bucketlist/Bucketlist/EditView.swift
+++ Bucketlist/Bucketlist/EditView.swift
@@ -8,9 +8,7 @@
 import SwiftUI
 
 struct EditView: View {
-    enum LoadingState {
-        case loading, loaded, failed
-    }
+    @StateObject private var viewModel = ViewModel()
     
     @Environment(\.dismiss) var dismiss
     
@@ -19,9 +17,6 @@ struct EditView: View {
     
     @State private var name: String
     @State private var description: String
-    
-    @State private var loadingState = LoadingState.loading
-    @State private var pages = [Page]()
     
     var body: some View {
         NavigationStack {
@@ -32,11 +27,11 @@ struct EditView: View {
                 }
                 
                 Section("Nearby...") {
-                    switch loadingState {
+                    switch viewModel.loadingState {
                         case .loading:
                             Text("Loading...")
                         case .loaded:
-                            ForEach(pages, id: \.pageid) { page in
+                            ForEach(viewModel.pages, id: \.pageid) { page in
                                 Text(page.title)
                                     .font(.headline)
                                 + Text(": ")
@@ -64,7 +59,7 @@ struct EditView: View {
                 }
             }
             .task {
-                await fetchNearbyPlaces()
+                await viewModel.fetchNearbyPlaces(location: location)
             }
         }
     }
@@ -75,25 +70,6 @@ struct EditView: View {
         
         _name = State(initialValue: location.name)
         _description = State(initialValue: location.description)
-    }
-    
-    func fetchNearbyPlaces() async {
-        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
-        
-        guard let url = URL(string: urlString) else {
-            print("Bad URL: \(urlString)")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let items = try JSONDecoder().decode(Result.self, from: data)
-            pages = items.query.pages.values.sorted()
-            loadingState = .loaded
-        } catch {
-            loadingState = .failed
-        }
-        
     }
 }
 
