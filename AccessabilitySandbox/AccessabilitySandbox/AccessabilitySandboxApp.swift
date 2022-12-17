258c6be8d595d4b55a20430940c47c29b155e86e
Daan Schutte <daan.schutte@icloud.com>
2022-12-17 13:59:48 +0300

    Day 74: Create project

---
+++ AccessabilitySandbox/AccessabilitySandbox/AccessabilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessabilitySandboxApp.swift
+//  AccessabilitySandbox
+//
+//  Created by Daan Schutte on 17/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AccessabilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
