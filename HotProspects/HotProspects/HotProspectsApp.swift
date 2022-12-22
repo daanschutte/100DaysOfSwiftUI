374e0f3a393b23b25bdd3d0c771927f753effc3d
Daan Schutte <daan.schutte@icloud.com>
2022-12-22 09:22:13 +0300

    Day 79: Create project

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Daan Schutte on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
