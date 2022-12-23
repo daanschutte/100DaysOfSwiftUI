657221db434ad40054c51f2e3db1f3113445b756
Daan Schutte <daan.schutte@icloud.com>
2022-12-23 10:29:58 +0300

    Day 82: Reset project

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -5,20 +5,12 @@
 //  Created by Daan Schutte on 22/12/2022.
 //
 
-import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
-    let possibleNumbers = Array(1...60)
-
     var body: some View {
-        Text(results)
-    }
-    
-    var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        return strings.joined(separator: ", ")
+        Text("Hello, world!")
+            .padding()
     }
 }
 
