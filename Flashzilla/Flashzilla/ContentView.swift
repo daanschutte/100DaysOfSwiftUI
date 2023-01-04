4fedbde83abc54ef4a17a0343386e4e1ec398b71
Daan Schutte <daan.schutte@icloud.com>
2023-01-04 18:55:55 +0300

    Day 87: Supporting specific accessibility needs with SwiftUI - part 2

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -7,21 +7,26 @@
 
 import SwiftUI
 
+func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+    if UIAccessibility.isReduceMotionEnabled {
+        return try body()
+    } else {
+        return try withAnimation(animation, body)
+    }
+}
+
 struct ContentView: View {
-    @Environment(\.accessibilityDifferentiateWithoutColor) var diffirentiateWithoutColour
+    @Environment(\.accessibilityReduceMotion) var reduceMotion
+    @State private var scale = 1.0
     
     var body: some View {
-        HStack {
-            if diffirentiateWithoutColour {
-                Image(systemName: "checkmark.circle")
+        Text("Hello, world!")
+            .scaleEffect(scale)
+            .onTapGesture {
+                withOptionalAnimation {
+                    scale *= 1.5
+                }
             }
-            
-            Text("Success")
-        }
-        .padding()
-        .background(diffirentiateWithoutColour ? .black : .green)
-        .foregroundColor(.white)
-        .clipShape(Capsule())
     }
 }
 
