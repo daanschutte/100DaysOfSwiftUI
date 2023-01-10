ae28c959f27225fa6bb40dc58fd68d2b755244c4
Daan Schutte <daan.schutte@icloud.com>
2023-01-10 19:46:58 +0300

    Day 97: Making NavigationView work in landscape

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,11 +7,21 @@
 
 import SwiftUI
 
+extension View {
+    @ViewBuilder func phoneOnlyNavigationView() -> some View {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            self.navigationViewStyle(.stack)
+        } else {
+            self
+        }
+    }
+}
+
 struct ContentView: View {
     let resorts: [Resort] = Bundle.main.decode("resorts.json")
     
     var body: some View {
-        NavigationStack {
+        NavigationView {
             List(resorts) { resort in
                 NavigationLink {
                     Text(resort.name)
@@ -35,7 +45,10 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Resorts")
+            
+            WelcomeView()
         }
+        .phoneOnlyNavigationView()
     }
 }
 
