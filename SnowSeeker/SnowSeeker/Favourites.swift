65c6c2afeb4893386ea9051ca07fc70fc3874cba
Daan Schutte <daan.schutte@icloud.com>
2023-01-11 09:34:03 +0300

    Day  99: Challenge 2

--- SnowSeeker/SnowSeeker/Favourites.swift
+++ SnowSeeker/SnowSeeker/Favourites.swift
@@ -8,32 +8,40 @@
 import Foundation
 
 class Favourites: ObservableObject {
-    private var resorts: Set<String>
-    private let saveKey = "Favourites"
+    private var favourites = Set<String>()
+    private let dataKey = "favourites.json"
     
     init() {
-        // load our save data
-        
-        resorts = []
+        loadData()
     }
     
     func contains(_ resort: Resort) -> Bool {
-        resorts.contains(resort.id)
+        favourites.contains(resort.id)
     }
     
     func addResort(_ resort: Resort) {
         objectWillChange.send()
-        resorts.insert(resort.id)
-        save()
+        favourites.insert(resort.id)
+        saveData()
     }
     
     func removeResort(_ resort: Resort) {
         objectWillChange.send()
-        resorts.remove(resort.id)
-        save()
+        favourites.remove(resort.id)
+        saveData()
     }
     
-    func save() {
-        // write out our data
+    func saveData() {
+        if let data = try? JSONEncoder().encode(favourites) {
+            UserDefaults.standard.set(data, forKey: dataKey)
+        }
+    }
+    
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: dataKey) {
+            if let decoded = try? JSONDecoder().decode(Set<String>.self, from: data) {
+                favourites = decoded
+            }
+        }
     }
 }
